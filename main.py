72be6146718a7124a4476f62e6ced603c3bb889a
Alexandra <superbiscuit@gmail.com>
2022-03-13 15:09:20 -0700

    adding different params

--- main.py
+++ main.py
@@ -6,7 +6,20 @@ from service.PostCache import PostCache
 import datetime
 
 #todo this is garbage and should be moved to another level
-negative_filter = ['lampwork lofts', 'macarthur commons', 'bakery lofts', 'the boise']
+negative_filter = ['studio']
+
+zippies = [97211,997218,97212,97213,97232,97214,97215,97202,97206]
+
+site = 'portland',
+area = 'mlt',
+category = 'apa'
+
+filters = {
+    'max_price': 1300,
+    'private_room': True,
+    'search_distance': 5,
+}
+
 
 if __name__ == '__main__':
     timestamp = datetime.datetime.now()
@@ -14,13 +27,20 @@ if __name__ == '__main__':
     print(timestamp) 
     print("-----------")
 
-    cl_h = CraigslistHousing(site='sfbay', area='eby', category='apa',
-                             filters={'max_price': 2500, 'private_room': True, 'query': 'loft',
-                                      'search_distance': 10, 'zip_code': 94612
-                                      })
-    results = cl_h.get_results(sort_by='newest', geotagged=True, include_details=True)
     cache = PostCache()
     email_service = EmailService()
+
+    for zip in zippies:
+        print("Searching zipcode: ", str(zip))
+        cl_h = CraigslistHousing(site, area, category,
+                             filters={
+                                'max_price': filters.get('max_price'), 
+                                'private_room': filters.get('private_room'),
+                                'search_distance': 1, 'zip_code': zip
+                                }
+                                )
+        results = cl_h.get_results(sort_by='newest', geotagged=True, include_details=True)
+
 
     should_send_emails = not cache.is_cache_empty()
     for result in results:
